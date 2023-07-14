b375117d32ea4251c48dd50230a786169acecadd
testgithubbi <123469328+testgithubbi@users.noreply.github.com>
2023-07-14 22:19:44 +0530

    task-4

---
+++ main.js
@@ -0,0 +1,59 @@
+var para1=document.getElementById("data1");
+	var para2=document.getElementById("data2");
+	var para3=document.getElementById("data3");
+	var para4=document.getElementById("data4");
+
+	const student={id:1,name:"Pari",Pincode:396375,Age:21};
+
+	para1.innerHTML=student.id;
+	para2.innerHTML=student.name;
+	para3.innerHTML=student.Pincode;
+	para4.innerHTML=student.Age;
+
+	var para5=document.getElementById("data5");
+	var para6=document.getElementById("data6");
+	var para7=document.getElementById("data7");
+	var para8=document.getElementById("data8");
+
+	const student1={id:2,name:"Priya",Pincode:123456,Age:25};
+
+	para5.innerHTML=student1.id;
+	para6.innerHTML=student1.name;
+	para7.innerHTML=student1.Pincode;
+	para8.innerHTML=student1.Age;
+
+	var para9=document.getElementById("data9");
+	var para10=document.getElementById("data10");
+	var para11=document.getElementById("data11");
+	var para12=document.getElementById("data12");
+
+	const student2={id:3,name:"jiya",Pincode:23456,Age:30};
+
+	para9.innerHTML=student2.id;
+	para10.innerHTML=student2.name;
+	para11.innerHTML=student2.Pincode;
+	para12.innerHTML=student2.Age;
+
+	var para13=document.getElementById("data13");
+	var para14=document.getElementById("data14");
+	var para15=document.getElementById("data15");
+	var para16=document.getElementById("data16");
+
+	const student3={id:4,name:"Palak",Pincode:345678,Age:35};
+
+	para13.innerHTML=student3.id;
+	para14.innerHTML=student3.name;
+	para15.innerHTML=student3.Pincode;
+	para16.innerHTML=student3.Age;
+
+	var para17=document.getElementById("data17");
+	var para18=document.getElementById("data18");
+	var para19=document.getElementById("data19");
+	var para20=document.getElementById("data20");
+
+	const student4={id:4,name:"Palak",Pincode:345678,Age:35};
+
+	para17.innerHTML=student4.id;
+	para18.innerHTML=student4.name;
+	para19.innerHTML=student4.Pincode;
+	para20.innerHTML=student4.Age;
